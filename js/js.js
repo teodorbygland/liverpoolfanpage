076fb44de766266c7008b71c64a5ffd010c2b5d8
teodorbygland <152858315+teodorbygland@users.noreply.github.com>
2023-12-07 10:37:17 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,5 @@
+alert("Hei! Og velkommen til verdens beste fotball lag sin fan website!");
+
+let navn = window.prompt("Først, hva heter den dårligeste klubben i England??");
+alert("Manchester United selvfølgelig! ");
+document.write(navn);
